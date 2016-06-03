b0e927f18549b3fd228bd9438526ad71b7aeb452
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-06-03 17:00:13 +0700

    Update test suite

--- tests/api/utils/pagination.js
+++ tests/api/utils/pagination.js
@@ -19,13 +19,26 @@ describe('api/utils/pagination.js', () => {
   it('should create only self and next links', () => {
     const page = 1
     const itemsPerPage = 5
-    const totalItems = 11
+    const totalItems = 10
     
     const { self, next, previous } = pagination(page, itemsPerPage, totalItems, 
-      `/api/v1/posts?page[size]=${itemsPerPage}`)
+      `/api/v1/posts`)
     
     expect(self).to.match(/(?=.*page\[number\]\=1)(?=.*page\[size\]\=5).*$/)
     expect(next).to.match(/(?=.*page\[number\]\=2)(?=.*page\[size\]\=5).*$/)
     expect(previous).to.be.undefined
+  })
+  
+  it('should create only self and previous links', () => {
+    const page = 3
+    const itemsPerPage = 5
+    const totalItems = 11
+    
+    const { self, next, previous } = pagination(page, itemsPerPage, totalItems,
+      `/api/v1/posts?page[number]=${page}`)
+    
+    expect(self).to.match(/(?=.*page\[number\]\=3)(?=.*page\[size\]\=5).*$/)
+    expect(next).to.be.undefined
+    expect(previous).to.match(/(?=.*page\[number\]\=2)(?=.*page\[size\]\=5).*$/)
   })
 })
