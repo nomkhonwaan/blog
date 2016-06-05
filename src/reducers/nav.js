cf5fb0022513058be02cd2130c6b6e9a2b18da4f
Natcha Luang - Aroonchai <me@nomkhonwaan.com>
2016-06-05 15:07:56 +0700

    Update nav reducer

--- src/reducers/nav.js
+++ src/reducers/nav.js
@@ -1,10 +1,19 @@
 import types from '../constants/actionTypes'
 
-export default (state, action) => {
+const initialState = {
+  isExpanded: false
+}
+
+export default (state = initialState, action) => {
   switch(action.type) {
-    case types.ON_CLICK_MENU_BUTTON:
+    case types.ON_CLICK_MENU_BUTTON: {
       return Object.assign({}, state, {
         isExpanded: action.isExpanded
       })
+    }
+    
+    default: {
+      return state
+    }
   }
 }
