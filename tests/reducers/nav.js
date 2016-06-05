99880dc7b13d50ac9934f8196f671a242144fb4d
Natcha Luang - Aroonchai <me@nomkhonwaan.com>
2016-06-05 13:47:15 +0700

    Move file to valid folder

---
+++ tests/reducers/nav.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import nav from '../../src/reducers/nav'
+
+describe('reducers/nav.js', () => {
+  it('should handle ON_CLICK_MENU_BUTTON by toggle isExpanded to "true"', () => {
+    
+  })
+})
