204d48dba743903b4b72e23a108e6a77ce4ccb02
Natcha Luang - Aroonchai <me@nomkhonwaan.com>
2016-06-05 13:52:42 +0700

    Add toggle false on click menu button

--- tests/actions/nav.js
+++ tests/actions/nav.js
@@ -3,8 +3,17 @@ import types from '../../src/constants/actionTypes'
 import { onClickMenuButton } from '../../src/actions/nav'
 
 describe('actions/nav.js', () => {
-  it('should toggle sidebar menu when click on menu button', () => {
+  it('should toggle state "true" when click on menu button', () => {
     const isExpanded = false 
+    
+    expect(onClickMenuButton(isExpanded)).to.deep.equal({
+      type: types.ON_CLICK_MENU_BUTTON,
+      isExpanded: ! isExpanded
+    })
+  })
+  
+  it('should toggle state "false" when click on menu button', () => {
+    const isExpanded = true 
     
     expect(onClickMenuButton(isExpanded)).to.deep.equal({
       type: types.ON_CLICK_MENU_BUTTON,
