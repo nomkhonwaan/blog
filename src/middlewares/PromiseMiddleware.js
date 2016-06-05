5fc058a93bfe894fa3f47bc25ede1cdc4b15bc38
Natcha Luang - Aroonchai <me@nomkhonwaan.com>
2016-06-06 06:00:21 +0700

    Create promise middleware

---
+++ src/middlewares/PromiseMiddleware.js
@@ -0,0 +1,12 @@
+export default () => {
+  return (next) => (action) => {
+    // const { promise, types, ...rest } = action
+    
+    // if ( ! promise) {
+    //   return next(action)
+    // }
+    
+    // const [ REQUEST, SUCCESS, FAILURE ] = types 
+    // next({ ...rest, type: REQUEST })
+  }
+}
