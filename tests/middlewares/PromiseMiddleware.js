4c078ea153df01f5d309d6705409eb1957ace429
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-06-06 10:38:17 +0700

    Update middleware test suite

--- tests/middlewares/PromiseMiddleware.js
+++ tests/middlewares/PromiseMiddleware.js
@@ -1,22 +1,11 @@
 import { expect } from 'chai'
+import configureMockStore from 'redux-mock-store'
 
 import types from '../../src/constants/ActionTypes'
-import promiseMiddleware from '../../src/middlewares/PromiseMiddleware'
+import PromiseMiddleware from '../../src/middlewares/PromiseMiddleware'
 
-const createFakeStore = (fakeData) => ({
-  getState() {
-    return fakeData
-  }
-})
-
-const dispatchWithStoreOf = (storeData, action) => {
-  let dispatched = null 
-  const dispatch = promiseMiddleware(createFakeStore(storeData))((actionAttempt) => {
-    dispatched = actionAttempt
-  })
-  dispatch(action)
-  return dispatched
-}
+const middlewares = [ PromiseMiddleware ]
+const mockStore = configureMockStore(middlewares)
 
 describe('middlewares/PromiseMiddleware.js', () => {
   it('should not dispatch if action does not have "promise" property', () => {
@@ -24,7 +13,90 @@ describe('middlewares/PromiseMiddleware.js', () => {
       type: types.ON_CLICK_MENU_BUTTON
     }
     
-    expect(dispatchWithStoreOf({}, action)).to.deep.equal(action)
+    const store = mockStore({})
+    
+    expect(store.dispatch(action)).to.deep.equal(action)
+  })
+  
+  it('should dispatch request state while calling promise action and return success state', () => {
+    const action = {
+      types: [ 
+        types.POSTS_REQUEST, 
+        types.POSTS_SUCCESS,
+        types.POSTS_FAILURE
+      ],
+      promise: Promise.resolve({
+        body: {
+          foo: 'bar'
+        }
+      })
+    }
+    
+    const store = mockStore({})
+    
+    const expectedActions = [
+      { type: types.POSTS_REQUEST },
+      { type: types.POSTS_SUCCESS, foo: 'bar' }
+    ]
+    
+    store.dispatch(action). 
+      then(() => {
+        expect(store.getActions()).to.deep.equal(expectedActions)
+      })
+  })
+  
+  it('should dispatch request state while calling promise action and return failure state, errors response acquired', () => {
+    const action = {
+      types: [
+        types.POSTS_REQUEST,
+        types.POSTS_SUCCESS,
+        types.POSTS_FAILURE
+      ],
+      promise: Promise.resolve({
+        errors: [{
+          foo: 'bar'
+        }]
+      })
+    }
+    
+    const store = mockStore({})
+    
+    const expectedActions = [
+      { type: types.POSTS_REQUEST },
+      { type: types.POSTS_FAILURE, foo: 'bar' }
+    ]
+    
+    store.dispatch(action). 
+      then(() => {
+        expect(store.getActions()).to.deep.equal(expectedActions)
+      })
+  })
+  
+  it('should dispatch request state while calling promise action and return failure state, an error has occurred', () => {
+    const action = {
+      types: [
+        types.POSTS_REQUEST,
+        types.POSTS_SUCCESS,
+        types.POSTS_FAILURE
+      ],
+      promise: Promise.reject({
+        errors: [{
+          foo: 'bar'
+        }]
+      })
+    }
+    
+    const store = mockStore({})
+    
+    const expectedActions = [
+      { type: types.POSTS_REQUEST },
+      { type: types.POSTS_FAILURE, foo: 'bar' }
+    ]
+    
+    store.dispatch(action). 
+      then(() => {
+        expect(store.getActions()).to.deep.equal(expectedActions)
+      })
   })
 })
 
