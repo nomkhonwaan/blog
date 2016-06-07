d450186d842845c981f06267d59d84cab6f462bf
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-06-07 15:47:32 +0700

    Update author module

---
+++ src/components/Author.jsx
@@ -0,0 +1,17 @@
+import React, { PropTypes } from 'react'
+
+const Author = () => {
+  const styles = require('../stylesheets/Author.scss')
+  
+  return (
+    <div className={ styles.author }>
+      <a className={ styles.avatar } href="https://www.nomkhonwaan.com">
+        <img src={ require('../images/nomkhonwaan.jpg') } alt="Natcha Luang - Aroonchai" />
+      </a>
+      <h2 className={ styles.description }>Trust me I'm Petdo</h2>
+      <div className={ styles.location }>Bangkok, Thailand</div>
+    </div>
+  )
+}
+
+export default Author
