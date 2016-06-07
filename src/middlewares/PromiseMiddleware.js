3a98b1a59b67ec8c6d265cd52ef545955f683b58
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-06-07 15:06:00 +0700

    Add received at property

--- src/middlewares/PromiseMiddleware.js
+++ src/middlewares/PromiseMiddleware.js
@@ -13,16 +13,31 @@ export default (store) => {
       then(
         ({ body }) => {
           if (body.errors) {
-            return next({ ...rest, ...body, type: FAILURE })
+            return next({ 
+              ...rest, 
+              ...body,
+              receivedAt: Date.now(), 
+              type: FAILURE 
+            })
           }
-          return next({ ...rest, ...body, type: SUCCESS })
+          return next({ 
+            ...rest, 
+            ...body, 
+            receivedAt: Date.now(),
+            type: SUCCESS 
+          })
         },
         (err) => {
-          return next({ ...rest, errors: [{
-            status: 400,
-            title: 'An error has occurred',
-            detail: err.toString()
-          }], type: FAILURE })
+          return next({ 
+            ...rest, 
+            errors: [{
+              status: 400,
+              title: 'An error has occurred',
+              detail: err.toString()
+            }], 
+            receivedAt: Date.now(),
+            type: FAILURE 
+          })
         }
       )
   }
