b98a3aed5ef361fd876192e18ffe518a5610759c
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-06-07 10:18:58 +0700

    Apply asyncConnect to container

--- src/containers/Posts.jsx
+++ src/containers/Posts.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { asyncConnect } from 'redux-connect'
 
-const Posts = () => {
+import { fetchPosts } from '../actions/PostsActions'
+
+const Posts = ({ entities }) => {
   return (
     <div></div>
   )
 }
 
-export default Posts 
+export default asyncConnect([{
+  key: 'posts',
+  promise: ({ store: { dispatch, getState } }) => {
+    return dispatch(fetchPosts(
+      getState().posts.page,
+      getState().posts.itemsPerPage
+    ))
+  }
+}], (state, ownProps) => {
+  return {
+    entities: state.entities,
+    ...state.posts
+  }
+})(Posts)
