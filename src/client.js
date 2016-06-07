69faffb7f46701c719760931a673016382cd5d31
Natcha Luang - Aroonchai <me@nomkhonwaan.com>
2016-06-07 20:42:33 +0700

    Create client.js

---
+++ src/client.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { applyMiddleware, createStore, compose } from 'redux'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { applyRouterMiddleware, browserHistory, Router } from 'react-router'
+import { ReduxAsyncConnect } from 'redux-connect'
+import { persistState } from 'redux-devtools'
+import { routerMiddleware, syncHistoryWithStore } from 'react-router-redux'
+import useScroll from 'react-router-scroll'
+
+import PromiseMiddleware from './middlewares/PromiseMiddleware'
+import reducers from './reducers'
+import routes from './routes'
+
+const initialState = window.__INITIAL_STATE__
+const store = createStore(
+  reducers,
+  initialState,
+  compose(
+    applyMiddleware(
+      PromiseMiddleware,
+      routerMiddleware(browserHistory)
+    )
+  ),
+  window.devToolsExtension && window.devToolsExtension(),
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+)
+
+const history = syncHistoryWithStore(browserHistory, store)
+
+render(
+  <Provider store={ store }>
+    <Router render={ (renderProps) => {
+      return (
+        <ReduxAsyncConnect { ...renderProps }
+          render={ applyRouterMiddleware(useScroll()) } />
+      )
+    } } history={ history }>
+      { routes }
+    </Router>
+  </Provider>
+)
