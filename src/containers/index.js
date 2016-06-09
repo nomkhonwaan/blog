77409a31c42988c4e9e521567a06603ca8855411
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-06-09 08:42:16 +0700

    Add post route, component

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,7 @@
+import Post from './Post'
 import Posts from './Posts'
 
-export { Posts }
+export { 
+  Post,
+  Posts 
+}
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
 import { App } from './components'
-import { Posts } from './containers'
+import { Post, Posts } from './containers'
 
 export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={Posts} />
+  <Route path="/" component={ App }>
+    <IndexRoute component={ Posts } />
+    <Route path="/:year/:month/:date/:slug" component={ Post } />
   </Route>
 )
