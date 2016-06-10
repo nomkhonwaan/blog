37e21f38aa5dbb5822e37c4e961a0b3f588149c0
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-06-10 20:31:02 +0700

    Update defaults.js

--- src/config/defaults.js
+++ src/config/defaults.js
@@ -1,7 +1,7 @@
 import config from 'cloud-env'
 
 export default {
-  PORT: config.get('PORT') || process.env.PORT || 8080,
-  IP: config.get('IP') || '0.0.0.0',
-  MONGODB_URI: config.get('MONGODB_DB_URL') || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test'
+  PORT: process.env.PORT || config.get('PORT', 8080),
+  IP: config.get('IP', '0.0.0.0'),
+  MONGODB_URI: process.env.MONGODB_URI || config.get('MONGODB_DB_URL', 'mongodb://127.0.0.1:27017/test')
 }
