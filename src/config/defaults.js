baa53bfa5b1684bd50b8bd64c07343788f042541
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-06-10 20:02:36 +0700

    Update defaults.js

--- src/config/defaults.js
+++ src/config/defaults.js
@@ -1,4 +1,9 @@
+import config from 'cloud-env'
+
 export default {
-  port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-  ip: process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+  port: config.get('PORT') || process.env.PORT || 8080,
+  ip: config.get('IP') || '0.0.0.0',
+  mongo: {
+    url: config.get('MONGODB_DB_URL') || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test'
+  }
 }
