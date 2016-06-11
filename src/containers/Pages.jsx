b870520fdb6fdbb6c1edd001bf6f84f5e4e54a53
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-06-11 13:56:46 +0700

    Add pages component

---
+++ src/containers/Pages.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pages = () => {
+  return (
+    <div>Page</div>
+  )
+}
+
+export default Pages
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,15 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
 import { App } from './components'
-import { Post, Posts } from './containers'
+import { 
+  Pages,
+  Post, 
+  Posts } from './containers'
 
 export default (
   <Route path="/" component={ App }>
     <IndexRoute component={ Posts } />
     <Route path="/:year/:month/:date/:slug" component={ Post } />
+    <Route path="/pages/:page" component={ Pages } />
   </Route>
 )
