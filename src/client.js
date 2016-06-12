4fb0b46888829fb6b325dfa666cad5763eacdfeb
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-06-13 06:20:31 +0700

    Disable redux devtools for fixing invalid object on mobile browser

--- src/client.js
+++ src/client.js
@@ -16,14 +16,16 @@ const initialState = decodeURIComponent(window.__INITIAL_STATE__)
 const store = createStore(
   reducers,
   initialState,
-  compose(
+  // compose(
     applyMiddleware(
       PromiseMiddleware,
       routerMiddleware(browserHistory)
     ),
-    window.devToolsExtension && window.devToolsExtension(),
-    persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
-  ),
+    // (window.devToolsExtension 
+    //   ? window.devToolsExtension()
+    //   : null),
+  //   persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+  // ),
 )
 
 const history = syncHistoryWithStore(browserHistory, store)
