21385d08e6c8820c71e18f182c616b57f5295c5f
Natcha Luang - Aroonchai <me@nomkhonwaan.com>
2016-06-15 20:55:41 +0700

    Fix invalid config

--- src/config/defaults.js
+++ src/config/defaults.js
@@ -4,6 +4,6 @@ export default {
   PORT: process.env.PORT || 8080,
   IP:  '0.0.0.0',
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test',
-  REDIS_URL: process.env.REDIS_URL || 'edis://localhost:6379',
+  REDIS_URL: process.env.REDIS_URL || 'redis://localhost:6379',
   REDIS_SECRET: process.env.REDIS_SECRET || 'redisSecretString'
 }
