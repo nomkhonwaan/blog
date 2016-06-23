a4d4da94cadff81bdab6954264577c0463abc19c
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-06-23 10:40:01 +0700

    Code format

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,22 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
-import { changePage } from './actions/PostsActions'
 import { App } from './components'
-import {
-  Post, 
-  Posts } from './containers'
+import { Post, Posts } from './containers'
+import { changePage } from './actions/PostsActions'
 
 export default ({ dispatch }) => {
   return (
     <Route path="/" component={ App }>
       <IndexRoute component={ Posts } />
       <Route path="/:year/:month/:date/:slug" component={ Post } />
-      <Route path="/pages/:page" getComponent={ 
+      <Route path="/pages/:page" getComponent={
         (nextState, callback) => {
           const { params } = nextState
 
-          dispatch(
-            changePage(
-              parseInt(params.page)
-            )
-          )
-          
+          dispatch(changePage(parseInt(params.page)))
           callback(null, Posts)
-        } 
+        }
       } />
     </Route>
   )
