39e477b23930e971b4c93f86d714100c760da657
Natcha Luang - Aroonchai <me@nomkhonwaan.com>
2016-06-26 22:27:15 +0700

    Found the way to test React isomorphic component

---
+++ tests/components/Author.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai'
+import path from 'path'
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
+
+import WebpackIsomorphicTools from 'webpack-isomorphic-tools'
+import webpackIsomorphicToolsConfiguration from '../../src/webpack-isomorphic-tools-configuration'
+
+import Author from '../../src/components/Author'
+
+const webpackIsomorphicTools = new WebpackIsomorphicTools(webpackIsomorphicToolsConfiguration)
+
+describe('components/Author.jsx', () => {
+  it('should render correctly', (done) => {
+    webpackIsomorphicTools.server(path.resolve(__dirname, '..', '..'), () => {
+      done()
+    })
+  })
+})
