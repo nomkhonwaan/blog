dd35952e37860208703aa88d4e8d0ed1557beee9
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-06-27 11:47:18 +0700

    Using Docker v2 config instead

--- src/config/defaults.js
+++ src/config/defaults.js
@@ -3,7 +3,7 @@ import config from 'cloud-env'
 export default {
   PORT: process.env.PORT || 8080,
   IP:  '0.0.0.0',
-  MONGODB_URI: `mongodb://${process.env.MONGO_PORT_27017_TCP_ADDR}:${process.env.MONGO_PORT_27017_TCP_PORT}/nomkhonwaan_com`,
-  REDIS_URL: `redis://${process.env.REDIS_PORT_6379_TCP_ADDR}:${process.env.REDIS_PORT_6379_TCP_PORT}`,
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/nomkhonwaan_com',
+  REDIS_URL: process.env.REDIS_URL || 'redis://localhost:6379',
   REDIS_SECRET: process.env.REDIS_SECRET || 'redisSecretString'
 }
