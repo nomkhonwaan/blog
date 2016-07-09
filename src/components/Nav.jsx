17fb8554db88a8c59024593dd07114d9ada6b289
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-07-09 20:47:45 +0700

    Move all menus to reducer as initial state

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,29 +4,7 @@ import { connect } from 'react-redux'
 
 import { onClickMenuButton } from '../actions/NavActions'
 
-const Nav = ({ dispatch, isExpanded }) => {
-  const menuItems = [{
-    title: 'Home',
-    href: '//www.nomkhonwaan.com',
-    iconClass: 'fa fa-fw fa-home'
-  }, {
-    title: 'GitHub',
-    href: '//github.com/nomkhonwaan',
-    iconClass: 'fa fa-fw fa-github-square'
-  }, {
-    title: 'Twitter',
-    href: '//twitter.com/nomkhonwaan',
-    iconClass: 'fa fa-fw fa-twitter-square'
-  }, {
-    title: 'LinkedIn',
-    href: '//linkedin.com/in/nomkhonwaan',
-    iconClass: 'fa fa-fw fa-linkedin-square' 
-  }, {
-    title: 'RSS',
-    href: '//www.nomkhonwaan.com/rss',
-    iconClass: 'fa fa-fw fa-rss-square' 
-  }]
-  
+export const Nav = ({ dispatch, isExpanded, menuItems }) => {
   return (
     <nav className="layout-nav">
       <ul className={ classNames(
--- src/reducers/nav.js
+++ src/reducers/nav.js
@@ -1,7 +1,28 @@
 import types from '../constants/ActionTypes'
 
-const initialState = {
-  isExpanded: false
+export const initialState = {
+  isExpanded: false,
+  menuItems: [{
+    title: 'Home',
+    href: '//www.nomkhonwaan.com',
+    iconClass: 'fa fa-fw fa-home'
+  }, {
+    title: 'GitHub',
+    href: '//github.com/nomkhonwaan',
+    iconClass: 'fa fa-fw fa-github-square'
+  }, {
+    title: 'Twitter',
+    href: '//twitter.com/nomkhonwaan',
+    iconClass: 'fa fa-fw fa-twitter-square'
+  }, {
+    title: 'LinkedIn',
+    href: '//linkedin.com/in/nomkhonwaan',
+    iconClass: 'fa fa-fw fa-linkedin-square' 
+  }, {
+    title: 'RSS',
+    href: '//www.nomkhonwaan.com/rss',
+    iconClass: 'fa fa-fw fa-rss-square' 
+  }]
 }
 
 export default (state = initialState, action) => {
