e00aca506c0151804756d9f1180f56fbf147f3b0
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-07-09 20:48:23 +0700

    Update test nav reducer

--- test/reducers/nav.js
+++ test/reducers/nav.js
@@ -1,26 +1,24 @@
 import { expect } from 'chai'
 
 import types from '../../src/constants/ActionTypes'
-import reducer from '../../src/reducers/nav'
+import reducer, { initialState } from '../../src/reducers/nav'
 
 describe('reducers/nav.js', () => {
   it('should handle ON_CLICK_MENU_BUTTON by toggle isExpanded to "true"', () => {
     expect(reducer(undefined, {
       type: types.ON_CLICK_MENU_BUTTON,
       isExpanded: true
-    })).to.deep.equal({
+    })).to.deep.equal(Object.assign({}, initialState, {
       isExpanded: true
-    })
+    }))
   })
   
   it('should handle ON_CLICK_MENU_BUTTON by toggle isExpanded to "false"', () => {
-    expect(reducer({
-      isExpanded: true
-    }, {
+    expect(reducer(undefined, {
       type: types.ON_CLICK_MENU_BUTTON,
       isExpanded: false
-    })).to.deep.equal({
+    })).to.deep.equal(Object.assign({}, initialState, {
       isExpanded: false
-    })
+    }))
   })
 })
