270767196935888642420c7c0c617ef1b47352ca
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-07-18 16:15:17 +0700

    Create footer component test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Footer = () => {
+export const Footer = () => {
   return (
     <footer className="layout-footer">
       &copy;&nbsp;{new Date().getFullYear()}&nbsp;&middot;&nbsp;<strong className="copyright">www.nomkhonwaan.com</strong>&nbsp;&middot;&nbsp;Powered by&nbsp;<a href="https://facebook.github.io/react/" className="poweredby" title="React"><strong>React</strong></a>
---
+++ test/components/Footer.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+import React from 'react'
+import { connect } from 'react-redux'
+
+import { Footer } from '../../src/components/Footer'
+
+describe('components/Footer.jsx', () => {
+  it('should render `.layout-footer` component correctly', () => {
+    const wrapper = shallow(<Footer />)
+
+    expect(wrapper.find('.layout-footer')).to.have.length(1)
+    expect(wrapper.find('.layout-footer').text()).to.match(new RegExp(`${new Date().getFullYear()}`));
+  })
+})
