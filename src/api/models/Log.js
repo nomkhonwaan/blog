6f2b8c2c6c8a7c47128a219241707c12633b0566
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-07-21 14:48:55 +0700

    :sunglasses: Temporary disable Elasticsearch

--- src/api/models/Log.js
+++ src/api/models/Log.js
@@ -7,40 +7,40 @@ const Log = new mongoose.Schema({
   msg: {
     type: String,
     required: true,
-    es_indexed: true
+    // es_indexed: true
   },
   level: {
     type: Number,
     required: true,
-    es_indexed: true
+    // es_indexed: true
   },
   name: {
     type: String,
     required: true,
-    es_indexed: true
+    // es_indexed: true
   },
   time: {
     type: Date,
     required: true,
-    es_indexed: true
+    // es_indexed: true
   },
   res : {
     type: Object,
-    es_indexed: true
+    // es_indexed: true
   },
   req : {
     type: Object,
-    es_indexed: true
+    // es_indexed: true
   }
 }, {
   collection: 'logs'
 })
 
-Log.plugin(mongoosastic, {
-  hosts: [
-    config.ELASTICSEARCH_URI
-  ]
-})
+// Log.plugin(mongoosastic, {
+//   hosts: [
+//     config.ELASTICSEARCH_URI
+//   ]
+// })
 
 export default (mongoose.models.Log
   ? mongoose.model('Log')
--- src/config.js
+++ src/config.js
@@ -7,5 +7,5 @@ export default {
   MONGODB_URI:       process.env.MONGODB_URI || 'mongodb://mongo-primary:27017/nomkhonwaan_com,mongo-secondary:27017,mongo-tertiary:27017',
 
   // Elasticsearch
-  ELASTICSEARCH_URI: process.env.ELASTICSEARCH_URI || 'http://elasticsearch:9200'
+  // ELASTICSEARCH_URI: process.env.ELASTICSEARCH_URI || 'http://elasticsearch:9200'
 }
