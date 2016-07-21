a35414db5717ed081427228f33306a1b2adb3f7e
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-07-21 09:29:50 +0700

    :wrench: Write log directly to Elasticsearch using Mongoosastic

--- src/api/models/Log.js
+++ src/api/models/Log.js
@@ -1,31 +1,48 @@
 import mongoose from 'mongoose'
+import mongoosastic from 'mongoosastic'
+
+import config from '../../config'
+
+const Log = new mongoose.Schema({
+  msg: {
+    type: String,
+    required: true,
+    es_indexed: true
+  },
+  level: {
+    type: Number,
+    required: true,
+    es_indexed: true
+  },
+  name: {
+    type: String,
+    required: true,
+    es_indexed: true
+  },
+  time: {
+    type: Date,
+    required: true,
+    es_indexed: true
+  },
+  res : {
+    type: Object,
+    es_indexed: true
+  },
+  req : {
+    type: Object,
+    es_indexed: true
+  }
+}, {
+  collection: 'logs'
+})
+
+Log.plugin(mongoosastic, {
+  hosts: [
+    config.ELASTICSEARCH_URI
+  ]
+})
 
 export default (mongoose.models.Log
   ? mongoose.model('Log')
-  : mongoose.model('Log', new mongoose.Schema({
-    msg: {
-      type: String,
-      required: true
-    },
-    level: {
-      type: Number,
-      required: true
-    },
-    name: {
-      type: String,
-      required: true
-    },
-    time: {
-      type: Date,
-      required: true
-    },
-    res : {
-      type: Object
-    },
-    req : {
-      type: Object
-    }
-  }, {
-    collection: 'logs'
-  }))
+  : mongoose.model('Log', Log)
 )
--- src/config.js
+++ src/config.js
@@ -1,8 +1,11 @@
 export default {
   // Express.js
-  PORT:           process.env.PORT || 8080,
-  SESSION_SECRET: process.env.SECRET || 'sessionSecretString',
+  PORT:              process.env.PORT || 8080,
+  SESSION_SECRET:    process.env.SECRET || 'sessionSecretString',
 
   // MongoDB
-  MONGODB_URI:    process.env.MONGODB_URI || 'mongodb://mongo:27017/nomkhonwaan_com'
+  MONGODB_URI:       process.env.MONGODB_URI || 'mongodb://mongo:27017/nomkhonwaan_com',
+
+  // Elasticsearch
+  ELASTICSEARCH_URI: process.env.ELASTICSEARCH_URI || 'http://elasticsearch:9200'
 }
