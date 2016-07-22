a3848b4043af7387dde190f7cb6b4dcbf43b46d4
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-07-22 21:11:59 +0700

    :bangbang: Permanently remove MongoDB replica config

--- scripts/runReplicateSetup.sh
+++ scripts/runReplicateSetup.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
-mongo --host mongo_primary --port 27017 <<EOF
+sleep 10
+
+mongo --host mongo-primary --port 27017 <<EOF
   rs.initiate();
   rs.reconfig({
     "_id": "nomkhonwaan_com", 
     "version": 1, 
     "members": [{
       "_id": 0,
-      "host": "mongo_primary:27017", 
+      "host": "mongo-primary:27017", 
       "priority": 1
     }, {
       "_id": 1, 
-      "host": "mongo_secondary:27017",
-      "priority": 0
-    }, {
-      "_id": 2,
-      "host": "mongo_tertiary:27017",
+      "host": "mongo-secondary:27017",
       "priority": 0
     }]
   }, {
--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export default {
   SESSION_SECRET:    process.env.SECRET || 'sessionSecretString',
 
   // MongoDB
-  MONGODB_URI:       process.env.MONGODB_URI || 'mongodb://mongo-primary:27017/nomkhonwaan_com,mongo-secondary:27017,mongo-tertiary:27017',
+  MONGODB_URI:       process.env.MONGODB_URI || 'mongodb://mongo/nomkhonwaan_com',
 
   // Elasticsearch
   // ELASTICSEARCH_URI: process.env.ELASTICSEARCH_URI || 'http://elasticsearch:9200'
