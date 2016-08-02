2b8b4d08f0334d65f0f9515108a367796cb16b0a
Natcha Luang - Aroonchai <me@nomkhonwaan.com>
2016-08-03 05:58:14 +0700

    Fix Pagination test

--- tests/mocha/components/Pagination.test.js
+++ tests/mocha/components/Pagination.test.js
@@ -17,7 +17,7 @@ describe('component/Pagination', () => {
 
     expect(wrapper.find('.pagination')).to.have.length(1)
     expect(wrapper.find('.previous').text()).to.equal('')
-    expect(wrapper.find('.next').text()).to.equal('Older »')
+    expect(wrapper.find('.next').text()).to.match(/Older/)
     expect(wrapper.find('.counter').text()).to.equal('Page 1 of 3')
   })
 
@@ -27,30 +27,30 @@ describe('component/Pagination', () => {
       links={ {
         self: 'testSelfUrl',
         previous: 'testPreviousUrl'
-      } } 
+      } }
       page={ 3 }
       totalItems={ 11 } />)
-    
+
     expect(wrapper.find('.pagination')).to.have.length(1)
-    expect(wrapper.find('.previous').text()).to.equal('« Newer')
+    expect(wrapper.find('.previous').text()).to.match(/Newer/)
     expect(wrapper.find('.next').text()).to.equal('')
     expect(wrapper.find('.counter').text()).to.equal('Page 3 of 3')
   })
 
   it('should return `.pagination` both of previous and next button and show page 2 of 3 pages', () => {
-    const wrapper = mount(<Pagination 
+    const wrapper = mount(<Pagination
       itemsPerPage={ 5 }
       links={ {
         self: 'testSelfUrl',
         next: 'testNextUrl',
         previous: 'testPreviousUr'
-      } } 
+      } }
       page={ 2 }
       totalItems={ 11} />)
-    
+
     expect(wrapper.find('.pagination')).to.have.length(1)
-    expect(wrapper.find('.previous').text()).to.equal('« Newer')
-    expect(wrapper.find('.next').text()).to.equal('Older »')
+    expect(wrapper.find('.previous').text()).to.match(/Newer/)
+    expect(wrapper.find('.next').text()).to.match(/Older/)
     expect(wrapper.find('.counter').text()).to.equal('Page 2 of 3')
   })
 })
